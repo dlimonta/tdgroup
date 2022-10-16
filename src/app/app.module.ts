e3d9b49e36d300d41272740c86262acd7e572493
Diego <diego@iMac-di-diego.local>
2022-10-16 22:40:55 +0200

    update home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./modules/home-page/home-page.component";
-import { LoginFormComponent } from './components/login-form/login-form.component';
+import { LoginFormComponent } from "./components/login-form/login-form.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
-    declarations: [AppComponent, HomePageComponent, LoginFormComponent],
+    declarations: [
+        AppComponent,
+        HomePageComponent,
+        LoginFormComponent,
+        HeaderComponent,
+    ],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
+    exports: [HeaderComponent],
 })
 export class AppModule {}
